e92b837c803dda7fc9d8e6c492aaf3e42dbd2f8b
adrian <adrian@ibadia.cat>
2024-10-21 13:54:42 +0200

    añadido deberes m6 y m8

---
+++ m6/tutorial2_4.php
@@ -0,0 +1,23 @@
+<?php
+
+$a = rand(100,999);
+
+$b = rand(100,999);
+
+$stra = (string) $a;
+$strb = (string) $b;
+
+$array_a = explode(separator: '',  string: $stra);
+$array_b = explode(separator: '',  string: $strb);
+
+
+echo $arrayt_a[0];
+echo $arrayt_b[0];
+
+
+/*if($array_a [2] == $array_b [2]){
+    echo "$a"."-"."$b"."---->$true";
+}else{
+    echo "$a"."-". "$b"."---->$false";
+}*/
+
