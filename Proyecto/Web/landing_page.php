e4d4969f931290ff52543f90a7cd2762b9d4bc7c
adrian <adrian.gil@ibadia.cat>
2024-10-28 20:55:09 +0100

    landing_page_en_php_y_form_de_login

---
+++ Proyecto/Web/landing_page.php
@@ -0,0 +1,51 @@
+<?php 
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing_page</title>
+    <link rel="stylesheet" href="landing_page.css">
+</head>
+<body>
+    <div class="menu">
+        <!-- Start nav -->
+        <nav id="menu">
+            <!-- Start menu -->
+            <ul>
+                <li><a href="#">Menú</a>
+                    <!-- Start menú desplegable -->
+                    <ul>
+                        <li><a href="#">Usuarios</a></li>
+                        <li><a href="#">Productos</a></li>
+                        
+                    </ul>
+                    <!-- End menú desplegable -->
+                </li>
+            </ul>
+            <!-- End menu -->
+        </nav>
+        <!-- End nav -->
+    </div>
+
+    <div class="landing_page_centro">
+        <img class="logo" src="../imagenes_web/Distritootaku_logo_2.png" alt="Logo">
+    </div>
+
+    <div class="divlogin">
+       <a href="login.php"><button type="button" class="login">Log in</button></a> 
+    </div>  
+
+    
+  
+
+
+
+
+</body>
+</html>
---
+++ Proyecto/Web/login.php
@@ -0,0 +1,28 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <form class ="form_login" action="/action_page.php">
+    <label for="fname">Primer nombre:</label><br>
+    <input type="text" id="fname" name="fname" placeholder="Nombre"><br>
+    <label for="lname">Apellidos:</label><br>
+    <input type="text" id="lname" name="lname" placeholder="Apellidos"><br>
+    <label for="lname">DNI:</label><br>
+    <input type="text" id="lname" name="lname" placeholder="DNI"><br>
+    <label for="lname">Correo/gmail:</label><br>
+    <input type="text" id="lname" name="lname" placeholder="correo"><br><br>
+    <input type="submit" value="Submit">
+    </form> 
+    
+</body>
+</html>
