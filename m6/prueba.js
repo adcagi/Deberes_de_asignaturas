960e786258592207f8dfadc0ad4a0596f7baa476
Adrian <adrian.gil@ibadia.cat>
2024-10-28 21:03:30 +0100

    prueba_explicacion

---
+++ m6/prueba.js
@@ -0,0 +1,5 @@
+let nombres = [nombres_con_a,nombres_con_c,nombres_con_b]
+nombres_con_a = ["adrian","alexander","antonio"]
+for (let i=0; i < nombres[nombres_con_a]; i++){
+    document.write(i)
+}
