ed34fda65f89441c88fe6d6de087b25a122934f3
adcagi <adrian.gil@ibadia.cat>
2024-10-29 17:11:51 +0100

    Update ruleta.php

--- m7/ruleta.php
+++ m7/ruleta.php
@@ -1,10 +1,27 @@
-<form action="ruleta2.php" method="post">
+<?php
+
+session_start();
+
+
+?>
+
+
+<img src="tapet_ruleta.png">
+
+<form action="ruleta2.php" method="get">
 
 	<select name="tipo-apuesta">
 		<option>sencilla</option>
 		<option>rojo-negro</option>		
 		<option>par-impar</option>
 		<option>cuadro</option>
+		<option>falta-pasa</option>
+		<option>docena</option>
+		<option>sexta</option>
+		<option>caballo</option>
+		<option>transversal</option>
+		<option>columna</option>
+		<option>rojo-negro</option>
 		<!-- Completar las 6 apuestas que faltan con los casos del switch -->
 
 	</select><br/>
@@ -13,3 +30,5 @@
 	<input type="submit" value="enviar" />
 
 </form>
+
+
