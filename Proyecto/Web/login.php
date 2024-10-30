90a6d40c5e887a3b46185940efa5fc1eb7ecd9e9
adcagi <adrian.gil@ibadia.cat>
2024-10-30 21:01:55 +0100

    Update login.php

--- Proyecto/Web/login.php
+++ Proyecto/Web/login.php
@@ -1,5 +1,5 @@
 <?php 
-
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -7,22 +7,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
     <title>Document</title>
 </head>
 <body>
 
-
-    <form class ="form_login" action="/action_page.php">
-    <label for="fname">Primer nombre:</label><br>
-    <input type="text" id="fname" name="fname" placeholder="Nombre"><br>
-    <label for="lname">Apellidos:</label><br>
-    <input type="text" id="lname" name="lname" placeholder="Apellidos"><br>
-    <label for="lname">DNI:</label><br>
-    <input type="text" id="lname" name="lname" placeholder="DNI"><br>
-    <label for="lname">Correo/gmail:</label><br>
-    <input type="text" id="lname" name="lname" placeholder="correo"><br><br>
-    <input type="submit" value="Submit">
-    </form> 
+    <div class="login">
+        <form class ="form_login" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>" >
+            <label for="fname">Primer nombre:</label><br>
+            <input type="text" id="nombre" name="nombre" placeholder="Nombre"><br>
+            <label for="lname">Apellidos:</label><br>
+            <input type="text" id="apellidos" name="apellidos" placeholder="Apellidos"><br>
+            <label for="lname">DNI:</label><br>
+            <input type="text" id="dni" name="dni" placeholder="DNI"><br>
+            <label for="lname">Correo/gmail:</label><br>
+            <input type="text" id="correo" name="correo" placeholder="correo"><br><br>
+            <label for="lname">Contraseña</label><br>
+            <input type="password" id="contrasenya" name="contrasenya" placeholder="****"><br><br>
+            <input type="submit" value="Submit">
+        </form> 
+    </div>
     
 </body>
 </html>
+
+<?php 
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $nombre = htmlspecialchars($_REQUEST['nombre']);
+    $apellidos = htmlspecialchars($_REQUEST['apellidos']);
+    $dni = htmlspecialchars($_REQUEST['dni']);
+    $correo = htmlspecialchars($_REQUEST['correo']);
+    
+    $psswd = htmlspecialchars($_REQUEST['contrasenya']);
+
+    if(empty($nombre) || empty($apellidos) || empty($dni) || empty($correo)){
+        echo 'alguno de los campos está vacío'; //
+    }else{
+        echo''.$nombre.''.$apellidos.''.$dni.''.$correo.''.$psswd.'';
+    }
+/************* PENDIENTE */
+/* Falta validar con BBDD */
+
+}
+
+
+?>
