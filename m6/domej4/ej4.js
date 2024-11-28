8fcfb2f4dee96a90e67f9f934196ca1c1a88c43b
Adrian <adrian.gil@ibadia.cat>
2024-11-28 17:19:21 +0100

    ej4_probando queryselector

--- m6/domej4/ej4.js
+++ m6/domej4/ej4.js
@@ -1,14 +1,25 @@
 
+let textoanterior = document.querySelector("#a")
+textoanterior2 = textoanterior.innerText
 function ocultarTexto(){
-    let element = document.getElementById("contenidos_1")
-    element.style = "display:none;"
-    if (element.style == "display:none;"){
-        element.style = "display:block;"
+    let element = document.querySelector("#p")
+    let element2 = document.querySelector("#a")
+    if (element.style.visibility == "visible" || element.style.visibility == ""){
+        element.style.visibility = "hidden";
+        element2.innerHTML = "Pinchaste aquí";
+    }else{
+        element.style.visibility = "visible";
+        element2.innerHTML = textoanterior2
     }
+
+    
 }
 
 
 
-let ocultar = document.getElementById("enlace_1") 
+let ocultar = document.getElementById("enlace_1");
 
-ocultar.addEventListener("click", ocultarTexto)
+ocultar.addEventListener("click", ocultarTexto);
+let element2 = document.getElementById("enlace_1")
+
+console.log(element2.innerText)
