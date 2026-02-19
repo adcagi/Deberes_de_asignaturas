ac359d4883af65d06ad8639dd0b86b77476c5e96
adrian <adrian.gil@ibadia.cat>
2026-02-19 18:51:08 +0100

    practica examen ra8 hecha

---
+++ 0613/api-laravel/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+
+Route::apiResource('events', EventController::class);
+
+Route::get('/', function () {
+    return view('welcome');
+});
--- 0613/mi_server_de_node/index.js
+++ 0613/mi_server_de_node/index.js
@@ -39,7 +39,7 @@ app.get('/test', (req, res) => {
 });
 
 app.post('/users', (req, res) => {
-    console.log("post users")
+  console.log("post users")
   const newUser = req.body;
   newUser.id = users.length + 1;
   users.push(newUser);
