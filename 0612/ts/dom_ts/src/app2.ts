ba098f7c87de0757fc42f93e665d7d34f9c72acd
adrian <adrian.gil@ibadia.cat>
2026-02-20 15:17:58 +0100

    pr1 casi acabada

--- 0612/ts/dom_ts/src/app2.ts
+++ 0612/ts/dom_ts/src/app2.ts
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     window.onload = function() {
 	// Numero de enlaces de la pagina
-
+		let element  = document.querySelectorAll('a');
+		console.log(element.length); 
 	// Direccion del penultimo enlace
-
+		let penultimo = element[element.length -2]
+		console.log(penultimo.getAttribute("href"))
 	// Numero de enlaces que apuntan a http://prueba
-
+		let c =0;
+		for(let i = 0; i < element.length; i++){
+			if(element[i].getAttribute("href") === "http://prueba/"){
+				c += 1;
+			}
+		}
+		console.log(c)
 	// Numero de enlaces del tercer parrafo
-
+		
 }
 })
